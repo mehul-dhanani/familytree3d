85ccc4182c49a797d8125eb84a81fe29a2caf805
Mehul Dhanani <mehul.dhanani@gmail.com>
2026-02-26 09:41:57 +1100

    Title Page Change

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Family Tree - Guttu is here</h1>
+      <h1>Family Tree - Beta</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
